fec66bf14e4b58976fcc5cd7ea0b0019c3c466dc
Flavian <flavian.leprettre@ito.am>
2024-12-10 18:50:28 +0100

    refactor: fix indentation

--- lnmarkets/user/get_user.py
+++ lnmarkets/user/get_user.py
@@ -4,7 +4,7 @@ from lnmarkets.user.types import User
 
 
 def get_user(client: LNMClient) -> User:
-	"""
-	@see https://docs.lnmarkets.com/api/operations/usergetuser
-	"""
-	return json.loads(client.request_api('GET', '/user', {}, True))
+  """
+  @see https://docs.lnmarkets.com/api/operations/usergetuser
+  """
+  return json.loads(client.request_api('GET', '/user', {}, True))
